b3f587287bb94045f530fc03035e579baecd0d49
Rohit Patidar <rohitpatidar583@gmail.com>
2024-12-02 18:01:18 +0530

    complete language screen

--- src/Screens/LanguageScreen.jsx
+++ src/Screens/LanguageScreen.jsx
@@ -11,10 +11,10 @@ const LanguageScreen = ({ onSelectLanguage }) => {
   };
 
   return (
-    <div className="min-w-screen min-h-screen bg-white flex flex-col justify-between bg-gradient-to-r from-[#99C3AF] from-opacity-90 to-[#FEF08A] to-opacity-80">
-        <IoArrowBack className='text-2xl mt-6 ml-6' />
-        <div className=' rounded-xl m-4 bg-white p-4 '>
-      <h1 className="text-3xl font-bold mb-8">Select Language</h1>
+    <div className="min-w-screen min-h-screen bg-white flex flex-col   bg-gradient-to-r from-[#99C3AF] to-[#FEF08A] p-4">
+        <IoArrowBack className='text-2xl my-4' />
+        <div className=' rounded-xl  bg-white p-4'>
+      <h1 className="mb-4 text-xl font-semibold text-gray-800">Select Language</h1>
       <p className=''>Choose your language prefrence to ensure a personalized and seemless expreience</p>
       <div className="mb-6">
         <label className="block mb-4 border rounded-2xl  m-4 border-black p-6">
@@ -40,7 +40,7 @@ const LanguageScreen = ({ onSelectLanguage }) => {
       </div>
     
     <Link to='number '>
-      <button className="bg-green-500 text-white py-2 px-4 rounded-2xl w-[250px] ml-6 mt-[250px]">Continue</button>
+      <button className="bg-green-500 text-white py-2 px-4 rounded-2xl w-[250px] ml-6 mt-[260px]">Continue</button>
       </Link>
       </div>
     </div>
