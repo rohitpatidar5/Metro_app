a5a55b8fdb9a583e18a84619bb389b3d05a7887f
Rohit Patidar <rohitpatidar583@gmail.com>
2024-12-05 16:12:29 +0530

    improve language page

--- src/Screens/LanguageScreen.jsx
+++ src/Screens/LanguageScreen.jsx
@@ -11,9 +11,11 @@ const LanguageScreen = ({ onSelectLanguage }) => {
   };
 
   return (
-    <div className="min-w-screen min-h-screen bg-white flex flex-col   bg-gradient-to-r from-[#99C3AF] to-[#FEF08A] p-4">
+    <div className="min-w-screen min-h-screen bg-white flex flex-col   bg-gradient-to-r from-[#99C3AF]/70 to-[#FEF08A]/70 p-4">
+        <Link to='/wel'>
         <IoArrowBack className='text-2xl my-4' />
-        <div className=' rounded-xl  bg-white p-4'>
+        </Link>
+        <div className=' rounded-2xl  bg-white p-4'>
       <h1 className="mb-4 text-xl font-semibold text-gray-800">Select Language</h1>
       <p className=''>Choose your language prefrence to ensure a personalized and seemless expreience</p>
       <div className="mb-6">
@@ -40,7 +42,7 @@ const LanguageScreen = ({ onSelectLanguage }) => {
       </div>
     
     <Link to='number '>
-      <button className="bg-green-500 text-white py-2 px-4 rounded-2xl w-[250px] ml-6 mt-[260px]">Continue</button>
+      <button className="bg-green-700 text-white py-2 px-4 rounded-2xl w-[250px] ml-6 mt-[260px]">Continue</button>
       </Link>
       </div>
     </div>
